e689548fdb76735ec89d8e2db243697e152b4d65
KBJ-55 <76529236+KBJ-55@users.noreply.github.com>
2023-05-13 14:47:25 +0100

    13 May 2023: Adjusted background colour on all activity.
    Removed API requirements from SleepActivity.java
    Turned off debug mode.
    
    Added additional strings to strings.xml
    Amended feedback email to snorlabshelp101@gmail.com (Correct now)

--- app/src/main/java/com/kristianjones/snorlabs/SleepActivity.java
+++ app/src/main/java/com/kristianjones/snorlabs/SleepActivity.java
@@ -290,7 +290,7 @@ public class SleepActivity extends AppCompatActivity {
             //DEBUG MODE - When just wanting to check whether code works, this will set the sleep
             // confidence level to 1. When not in DEBUG MODE, this will set the receiver to
             // X (95 as of 08/03/2023)
-            debugMode = true;
+            debugMode = false;
 
             if (debugMode) {
                 confLimit = 0;
